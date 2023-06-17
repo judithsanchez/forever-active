71c7b1b9b8e2a9e8f94165b495182b075677ce89
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-17 01:15:04 -0500

    Connected log with routes

--- client/src/components/auth.jsx
+++ client/src/components/auth.jsx
@@ -1,30 +1,31 @@
 import React, { createContext, useContext, useState } from 'react';
+import axios from 'axios';
 
 // React Router
 import { useNavigate, Navigate } from 'react-router-dom';
 
-const users = [
-  {
-    username: 'yuyi',
-    isAdmin: true,
-    password: '123',
-    favoriteWorkouts: [
-      'https://youtu.be/vHWvMTIGJEQ',
-      'https://youtu.be/cmRz6Q8DOrA',
-    ],
-  },
-  { username: 'irene', isAdmin: true, password: '123', favoriteWorkouts: [] },
-  { username: 'steph', isAdmin: false, password: '123', favoriteWorkouts: [] },
-  {
-    username: 'sofia',
-    isAdmin: false,
-    password: '123',
-    favoriteWorkouts: [
-      'https://youtu.be/qw32DPzSEHo',
-      'https://youtu.be/ZvQ-5ad-6Qo',
-    ],
-  },
-];
+// const users = [
+//   {
+//     username: 'yuyi',
+//     isAdmin: true,
+//     password: '123',
+//     favoriteWorkouts: [
+//       'https://youtu.be/vHWvMTIGJEQ',
+//       'https://youtu.be/cmRz6Q8DOrA',
+//     ],
+//   },
+//   { username: 'irene', isAdmin: true, password: '123', favoriteWorkouts: [] },
+//   { username: 'steph', isAdmin: false, password: '123', favoriteWorkouts: [] },
+//   {
+//     username: 'sofia',
+//     isAdmin: false,
+//     password: '123',
+//     favoriteWorkouts: [
+//       'https://youtu.be/qw32DPzSEHo',
+//       'https://youtu.be/ZvQ-5ad-6Qo',
+//     ],
+//   },
+// ];
 
 const AuthContext = React.createContext();
 
@@ -32,6 +33,7 @@ function AuthProvider({ children }) {
   const navigate = useNavigate();
 
   const [user, setUser] = useState(null);
+  const [data, setData] = useState(null);
 
   const signup = (username, password) => {
     if (users.some((existingUser) => existingUser.username === username)) {
@@ -45,26 +47,52 @@ function AuthProvider({ children }) {
     navigate('/login');
   };
 
-  const login = (username, password) => {
-    const user = users.find((user) => user.username === username);
+  const login = async (username, password) => {
+    try {
+      const { data } = await axios.post(
+        '/api/users/login',
+        {
+          username: username,
+          password: password,
+        },
+        {
+          method: 'POST',
+        }
+      );
 
-    if (!user) {
-      console.log('The user is not registered');
-      navigate('/signup');
-      return;
-    } else {
-      if (user.password !== password) {
-        console.log('Incorrect password');
-        return;
-      }
-      setUser(user);
+      //store it locally
+      localStorage.setItem('token', data.token);
+      console.log(data.message, data.token);
+
+      const requestedData = await requestData();
+      setUser(requestedData); // Update the user state
+    } catch (error) {
+      console.log(error);
+      setData(error.message);
     }
-
     navigate('/profile');
   };
 
+  const requestData = async () => {
+    try {
+      const { data } = await axios('/api/users/profile', {
+        headers: {
+          authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      });
+      console.log(data);
+      setData(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+      setData(error.message);
+      return null; // Return null or handle the error appropriately
+    }
+  };
+
   const logout = () => {
-    setUser(null);
+    localStorage.removeItem('token');
+    setData(null);
     navigate('/');
   };
 
